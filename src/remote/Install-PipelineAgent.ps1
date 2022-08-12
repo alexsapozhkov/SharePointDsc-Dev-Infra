993af705b191f62a94dcf8c035abca071046ed3d
Aleks <aleksandr.sapozhkov@soprasteria.com>
2022-08-12 14:08:22 +0200

    speeding up the github agent downloading

--- src/remote/Install-PipelineAgent.ps1
+++ src/remote/Install-PipelineAgent.ps1
@@ -6,6 +6,7 @@ param(
 
 if ($githubRunnerUrl -and $githubRunnerToken) {
     mkdir c:\actions-runner; cd c:\actions-runner
+    $ProgressPreference = 'SilentlyContinue';
     Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.294.0/actions-runner-win-x64-2.294.0.zip -OutFile actions-runner-win-x64-2.294.0.zip
     if((Get-FileHash -Path actions-runner-win-x64-2.294.0.zip -Algorithm SHA256).Hash.ToUpper() -ne '22295b3078f7303ffb5ded4894188d85747b1b1a3d88a3eac4d0d076a2f62caa'.ToUpper()){ throw 'Computed checksum did not match' }
     Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.294.0.zip", "$PWD")
